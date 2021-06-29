8623b7358d96e0709ef7991093ca978521c0435a
foryuxuan <i@littlecontrol.io>
2021-06-29 12:41:32 +0800

    一键启动脚本

--- utils/be/start.sh
+++ utils/be/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo $DIR
+python3 -m venv venv
+
+./venv/bin/pip install Flask lief flask_cors
+
 export FLASK_ENV=development
 export FLASK_APP=app
-flask run
+./venv/bin/flask run &
+cd ../fe/build
+echo $DIR
+../../be/venv/bin/python3 -m http.server 8081
